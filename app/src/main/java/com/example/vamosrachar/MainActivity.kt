af88cc93828ec82135256b0e862776b710ec34ba
Lucas Lima <lucaslima@fortestecnologia.com.br>
2021-07-05 23:45:12 -0300

    first commit

---
+++ app/src/main/java/com/example/vamosrachar/MainActivity.kt
@@ -0,0 +1,97 @@
+package com.example.vamosrachar
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.vamosrachar.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var valor : TextView
+    private lateinit var quantidade : TextView
+    private lateinit var valorPerCapita : TextView
+    private lateinit var tts : TextToSpeech
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        valor = findViewById(R.id.numberDecimal)
+        quantidade = findViewById(R.id.numberDecimal2)
+        valorPerCapita = findViewById(R.id.valor)
+        val botaoShare = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        val botaoTextToSpeech = findViewById<FloatingActionButton>(R.id.floatingActionButton2)
+
+        botaoShare.setOnClickListener{
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, valorPerCapita.text.toString())
+                type = "text/plain"
+            }
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+        }
+
+        tts = TextToSpeech(this, {})
+
+        botaoTextToSpeech.setOnClickListener{
+            val text = valorPerCapita.text.toString()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                tts.speak(text, TextToSpeech.QUEUE_FLUSH, null,"")
+            }
+        }
+
+
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    override fun onUserInteraction() {
+        if(valor.text.isNotEmpty() && quantidade.text.isNotEmpty()){
+            valorPerCapita.text = ((valor.text.toString()).toDouble() / (quantidade.text.toString()).toDouble()).toString()
+        }
+        super.onUserInteraction()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
